596f4e8c7e6f2959e8fa5e3c2f1b831a9ef8cfb2
schef <zvanstefan@gmail.com>
2021-01-15 10:38:19 +0100

    added tabs

--- projects/trombiano/addons/SimpleSampler/sample_resource.gd
+++ projects/trombiano/addons/SimpleSampler/sample_resource.gd
@@ -9,4 +9,4 @@ export(int) var octave := 4
 var value: int
 
 func initValue(calculator: NoteValueCalculator):
-  value = calculator.get_note_value(tone, octave)
+	value = calculator.get_note_value(tone, octave)
