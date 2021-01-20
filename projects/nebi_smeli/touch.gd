9a271fb71a324cd5efdef1f0184d338151158cc0
schef <zvanstefan@gmail.com>
2021-01-20 18:05:34 +0100

    added drag function

--- projects/nebi_smeli/touch.gd
+++ projects/nebi_smeli/touch.gd
@@ -5,6 +5,7 @@ var leftVerticalLine: float
 var rightVerticalLine: float
 var horizontalLine: float
 
+enum FingerAction {PRESS, RELEASE, DRAG}
 var fingerDict = {
 	0 : null,
 	1 : null,
@@ -23,27 +24,39 @@ func _ready():
 	update()
 	pass # Replace with function body.
 	
-func move_player(direction: String, action: bool, fingerIndex: int):
-	print("%s, %s, %s" % [direction, action, fingerIndex])
+func move_player(direction: String, fingerAction: int, fingerIndex: int):
+	print("direction[%s], fingerAction[%s], fingerIndex[%s]" % [direction, fingerAction, fingerIndex])
 	if (fingerIndex >= 2):
 		return
-	if (action):
-		if (fingerDict[fingerIndex] == null):
-			fingerDict[fingerIndex] = direction
-			Input.action_press(fingerDict[fingerIndex])
-	else:
-		Input.action_release(fingerDict[fingerIndex])
-		fingerDict[fingerIndex] = null
+	match fingerAction:
+		FingerAction.PRESS:
+			if (fingerDict[fingerIndex] == null):
+				fingerDict[fingerIndex] = direction
+				Input.action_press(fingerDict[fingerIndex])
+		FingerAction.DRAG:
+			if (fingerDict[fingerIndex] != direction and fingerDict[fingerIndex] != null):
+				Input.action_release(fingerDict[fingerIndex])
+				fingerDict[fingerIndex] = direction
+				Input.action_press(fingerDict[fingerIndex])
+		FingerAction.RELEASE:
+			if (fingerDict[fingerIndex] != null):
+				Input.action_release(fingerDict[fingerIndex])
+				fingerDict[fingerIndex] = null
 
+func get_touch_location(position: Vector2) -> String:
+		if position.x > (rightVerticalLine):
+			return("move_right")
+		elif position.x < (leftVerticalLine):
+			return("move_left")
+		else:
+			if position.y < (horizontalLine):
+				return("move_up")
+			else:
+				return("move_down")
+				
 func _input (event):
 	var screenSize = get_viewport().get_visible_rect().size
-	if event is InputEventScreenTouch:	
-		if event.position.x > (rightVerticalLine):
-			move_player("move_right", event.is_pressed(), event.index)
-		elif event.position.x < (leftVerticalLine):
-			move_player("move_left", event.is_pressed(), event.index)
-		else:
-			if event.position.y < (horizontalLine):
-				move_player("move_up", event.is_pressed(), event.index)
-			else:
-				move_player("move_down", event.is_pressed(), event.index)
+	if event is InputEventScreenTouch:
+		move_player(get_touch_location(event.position), FingerAction.PRESS if event.is_pressed() else FingerAction.RELEASE, event.index)
+	elif event is InputEventScreenDrag:
+		move_player(get_touch_location(event.position), FingerAction.DRAG, event.index)		
