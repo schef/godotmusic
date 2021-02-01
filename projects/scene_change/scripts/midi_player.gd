a7e5de10d3cf08074d693175d2d2793343505767
schef <zvanstefan@gmail.com>
2021-02-01 18:26:36 +0100

    added timeout

--- projects/scene_change/scripts/midi_player.gd
+++ projects/scene_change/scripts/midi_player.gd
@@ -1,50 +1,69 @@
 extends Multisampler
 
-const NOTE_DURATION = 1
+const NOTE_DURATION = 1000
+class NoteAction:
+	var midi: int
+	var status: bool
+	var startTimestamp: int
+	var timeout: int
+	
+var noteActionQueue = []
 
 func _ready():
-	print("ready")	
 	var n = NoteSample.new()
 	n.stream = preload("res://ogg/060.ogg")
 	n.value = 60
 	samples.append(n)
 
+func getMillis():
+	return OS.get_system_time_msecs()
+
+func millisPassed(timestamp: int):
+	return getMillis() - timestamp
+
 func _process(delta):
-	pass
+	for n in noteActionQueue:
+		if !n.status and getMillis() >= n.startTimestamp:
+			n.status = true
+			noteOn(n.midi)
+		elif n.status and millisPassed(n.startTimestamp) >= n.timeout:
+			n.status = false
+			noteOff(n.midi)
+	for i in range(len(noteActionQueue) - 1, -1, -1):
+		var n = noteActionQueue[i]
+		if !n.status and millisPassed(n.startTimestamp) >= n.timeout:
+			noteActionQueue.remove(i)
 
 func noteOn(note):
-	print("noteOn", note)
+	print("noteOn[%d]" % [note])
 	play_note(note)
 
 func noteOff(note):
-	print("noteOff", note)
+	print("noteOff[%d]" % [note])
 	release_note(note)
 
 func playMultipleNotesMelodicly(pitchList):
 	print("playMultipleNotesMelodicly", pitchList)
-	var pitchListCopy = [] + pitchList
-	var pitch = pitchListCopy.pop(0)
-	var midi = PitchParser.getMidiFromPitch(pitch)
-	noteOn(midi)
-	var timer = Timer.new()
-	timer.set_wait_time(NOTE_DURATION)
-	timer.connect("timeout", self, "NoteOff", [midi])
-	timer.start()
-	if len(pitchListCopy) > 0:
-		var timer2 = Timer.new()
-		timer2.set_wait_time(NOTE_DURATION)
-		timer2.connect("timeout", self, "playMultipleNotesMelodicly", [pitchListCopy])
-		timer2.start()
+	var currentTime = getMillis()
+	for pitch in pitchList:
+		var n = NoteAction.new()
+		n.midi = PitchParser.getMidiFromPitch(pitch)
+		n.status = false
+		n.startTimestamp = currentTime
+		n.timeout = NOTE_DURATION
+		currentTime = currentTime + NOTE_DURATION
+		noteActionQueue.append(n)
 
 func playMultipleNotesHarmonicly( pitchList):
 	print("playMultipleNotesHarmonicly", pitchList)
+	var currentTime = getMillis()
 	for pitch in pitchList:
-		var midi = PitchParser.getMidiFromPitch(pitch)
-		noteOn(midi)
-		var timer = Timer.new()
-		timer.set_wait_time(NOTE_DURATION)
-		timer.connect("timeout", self, "NoteOff", [midi])
-		timer.start()
+		var n = NoteAction.new()
+		n.midi = PitchParser.getMidiFromPitch(pitch)
+		n.status = false
+		n.startTimestamp = currentTime
+		n.timeout = NOTE_DURATION
+		noteActionQueue.append(n)
 
 func allNotesOff():
 	release_all()
